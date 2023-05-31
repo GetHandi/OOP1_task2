cb59abba99c2588ae82248b6ec7aac6c96e4d7dd
GetHandi <102155679+GetHandi@users.noreply.github.com>
2023-05-31 22:12:08 +0900

    Добавьте файлы проекта.

---
+++ Task_137e.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "calculation.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	unsigned n = 0;
+	cout << "Введите натуральное число n - ";
+	cin >> n;
+    
+	double* arr = new double[n];
+	arr = fill_array(arr, n);
+	cout << "Полученный масив рандомных:" << endl;
+	print_array(arr, n);
+	arr = calculate(arr, n);
+	cout << endl << "Ответ:" << endl;
+	print_array(arr, n);
+    delete[] arr;
+
+	
+}
---
+++ calculation.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+///возвращает массив со случайными действительными числами размера size до двух цифр после запятой;
+///a - ссылка на массив;
+///size - размер массива;
+double* fill_array(double* a, unsigned size)
+{
+	srand(time(NULL));
+	const int max_rand = 10000;
+	double lower_bound = 0;
+	double upper_bound = 100;
+	for (int i = 0; i < size; i++)
+	{
+		a[i] = lower_bound + (upper_bound - lower_bound) * (rand() % max_rand) / max_rand;
+	}
+	return a;
+}
+
+///выводит массив в консоль в формате "число * = **.**;
+///a - ссылка на массив;
+///size - размер массива;
+void print_array(double* a, unsigned size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << "число " << i+1 << " = " << a[i] << endl;
+	}
+}
+
+///возвращает фактоориал числа i;
+///i - целое положительное число;
+int factorial(unsigned i)
+{
+	if (i < 0)
+	{
+		return 0;
+	}
+	if (i == 1)
+	{
+		return 1;
+	}
+	else
+		return i * factorial(i - 1);
+
+}
+
+///возвращает массив с вычисленными выражениями: сумма числа массива + факториал его порядкового номера;
+///a - ссылка на массив;
+///size - размер массива;
+double* calculate(double* a, unsigned size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		a[i] += factorial(i + 1);
+	}
+	return a;
+}
